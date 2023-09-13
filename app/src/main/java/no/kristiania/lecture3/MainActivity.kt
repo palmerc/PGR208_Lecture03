d82d27e2a44120818876c344554879b5cc2d2db9
Cameron Lowell Palmer <cameron@promon.no>
2023-09-13 18:56:00 +0200

    The boxes demo

--- app/src/main/java/no/kristiania/lecture3/MainActivity.kt
+++ app/src/main/java/no/kristiania/lecture3/MainActivity.kt
@@ -1,31 +1,22 @@
-package no.kristiania.lecture3
+package no.kristiania.helloinsets
 
 import android.os.Bundle
-import android.widget.Space
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.statusBars
-import androidx.compose.foundation.layout.windowInsetsPadding
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -34,41 +25,60 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.painter.Painter
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.core.view.WindowCompat
-import no.kristiania.lecture3.ui.theme.Lecture3Theme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContent {
-            MyScreenContent()
+            BoxBoxBoxBoxBox()
         }
     }
 }
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun MyScreenContent() {
-    var mutableText by remember { mutableStateOf("1000000") }
+fun BoxBoxBoxBoxBox()
+{
+    var mutableText by remember { mutableStateOf("0") }
 
-    Column(horizontalAlignment = Alignment.CenterHorizontally) {
-        Image(painter = painterResource(id = R.drawable.horse01), contentDescription = "A horse" )
+    Column(modifier = Modifier.fillMaxHeight(), horizontalAlignment = Alignment.CenterHorizontally) {
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .background(Color.Magenta)
+                .weight(2f)
+                .height(300.dp),
+            contentAlignment = Alignment.Center
+        ) {
+            Box(modifier = Modifier.background(Color.Blue).size(200.dp))
+            Box(modifier = Modifier.background(Color.Red).size(100.dp))
 
-        OutlinedTextField(value = mutableText, onValueChange = {})
+            Box(modifier = Modifier.background(Color.Green).size(100.dp).align(Alignment.TopStart))
+            Box(modifier = Modifier.background(Color.Green).size(100.dp).align(Alignment.TopEnd))
+            Box(modifier = Modifier.background(Color.Green).size(100.dp).align(Alignment.BottomStart))
+            Box(modifier = Modifier.background(Color.Green).size(100.dp).align(Alignment.BottomEnd))
+
+        }
+
+        OutlinedTextField(
+            value = mutableText,
+            onValueChange = { newText ->
+                mutableText = newText
+            },
+            modifier = Modifier.background(Color.Red)
+        )
 
         Row(modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.Center) {
-            Row(modifier = Modifier.fillMaxWidth(.8f)) {
+            Row(horizontalArrangement = Arrangement.Center) {
                 Button(onClick = {
                     mutableText = (mutableText.toInt() + 1).toString()
                 }) {
                     Text(text = "Increment")
                 }
-                Spacer(modifier = Modifier.weight(1f))
+                Spacer(modifier = Modifier.size(12.dp))
                 Button(onClick = {
                     mutableText = (mutableText.toInt() - 1).toString()
                 }) {
@@ -77,11 +87,10 @@ fun MyScreenContent() {
             }
         }
     }
-
 }
 
 @Preview
 @Composable
-fun MyScreenContentPreview() {
-    MyScreenContent()
+fun BoxBoxBoxBoxBoxPreview() {
+    BoxBoxBoxBoxBox()
 }
